fed91a64a3d23948c4764687d11fa2d03b256664
Beatriz S.A <beatrizsilvaa1706@gmail.com>
2023-06-26 19:32:44 -0300

    iniciando projeto  java

---
+++ JAVA/criaConta.java
@@ -0,0 +1,27 @@
+package JAVA;
+
+public class criaConta {
+    public static void main (String [] args) {
+
+         Conta primeiraConta = new Conta();
+         primeiraConta.saldo = 200;
+
+         primeiraConta.saldo += 100;
+         System.out.println(primeiraConta.saldo);
+
+         
+         Conta segundaConta = new Conta();
+         segundaConta.saldo = 50;
+
+
+         System.out.println("Primeira conta tem " + primeiraConta.saldo);
+         System.out.println("Segunda conta tem " + segundaConta.saldo);
+
+         System.out.println(primeiraConta.agencia);
+
+
+
+
+
+    }
+}
