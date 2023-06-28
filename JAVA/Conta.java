0de07e7a5dec7a87b6ef66db20e3f02a5d74729e
Beatriz S.A <129802931+BeatrizSAA@users.noreply.github.com>
2023-06-28 12:50:16 +0000

    Desenvolvendo metodos java

--- JAVA/Conta.java
+++ JAVA/Conta.java
@@ -1,13 +1,29 @@
 package JAVA;
 public class Conta {
 
- double saldo;
- int agencia;
- int numero;
- String titular;
+      //atributos
+   double saldo; //0
+   int agencia; //0
+   int numero; //0
+   String titular; //string fica "??"
+
+ //comportamentos = metodo
+// o void é para explicar que não vai retornar nada apos o comportamento, o double pq é obrif=gatorio explicar o tipo de variavel e valor que estamos recebendo. 
+ //this é para especificar que é para o objeto chamado junto com a função, uma conta especifica
+   public void deposita (double valor){
+      this.saldo +=  valor;
+   }
+   public boolean saca(double valor ){
+      if (this.saldo >= valor){
+      this.saldo -= valor;
+      return true;
+      }
+
+      else { return false;
+      }
+
+   }
 
 
-
-
-    }
+}
 
