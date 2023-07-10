40b0c599aacb5aee5113c0f0aad4947864c6c281
Beatriz S.A <beatrizsilvaa1706@gmail.com>
2023-07-09 21:01:41 -0300

    novas aulas de java

---
+++ JavaAula/Java1.java
@@ -0,0 +1,55 @@
+package JavaAula;
+
+import java.util.Scanner;
+
+public class Java1 {
+
+    public static void main (String []args){
+     
+        //calculo de imc
+     /*    int peso;
+        double altura;
+        
+       System.out.println("Digite seu peso e sua altura: ");
+        Scanner teclado = new Scanner (System.in);
+        peso = teclado.nextInt();
+        altura = teclado.nextDouble(); //usar virgula por causa do windolows pt br
+
+        double imc = peso / (altura * altura);
+
+        System.out.println("Seu imc é " + imc); //sai numeros enormes
+        System.out.printf("Seu imc é %.2f", imc); //apenas 2 digitos apos a virgula
+ */
+ // palpite um numero ate acertar
+ /*
+     Scanner teclado = new Scanner (System.in);
+     int palpite;
+     int valorCorreto = 100;
+     System.out.println("digite seu palpite: ");
+     palpite = teclado.nextInt();
+
+     while (palpite != valorCorreto){
+     if (palpite > valorCorreto){
+     System.out.println("Um pouco menos");
+     } 
+     else {System.out.println("Um pouco mais");}
+
+     System.out.println("Digite outro palpite: ");
+     palpite = teclado.nextInt();
+}*/
+     //fiquei com prehuiça de copiar o ultimo que é de como fazer um for normal e ao contrario
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+    
+}
